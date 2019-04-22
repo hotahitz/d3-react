ba1e6b116cd7fd7dbbe486f9ebfbcc1cfa4f6462
hotahitz <hiteshhota01@gmail.com>
2019-04-22 17:13:34 +0530

    tried vanilla d3 and gathered some resources

---
+++ vanilla/profile.js
@@ -0,0 +1,134 @@
+profile = [
+  {
+    test: 1,
+    paper: [
+      {
+        qstn: 1,
+        tag: "P",
+        ans: 1
+      },
+      {
+        qstn: 2,
+        tag: "P",
+        ans: 1
+      },
+      {
+        qstn: 3,
+        tag: "P",
+        ans: 0
+      },
+      {
+        qstn: 4,
+        tag: "P",
+        ans: 0
+      },
+      {
+        qstn: 5,
+        tag: "C",
+        ans: 1
+      },
+      {
+        qstn: 6,
+        tag: "C",
+        ans: 1
+      },
+      {
+        qstn: 7,
+        tag: "C",
+        ans: 0
+      },
+      {
+        qstn: 8,
+        tag: "C",
+        ans: 1
+      },
+      {
+        qstn: 9,
+        tag: "M",
+        ans: 1
+      },
+      {
+        qstn: 10,
+        tag: "M",
+        ans: 0
+      },
+      {
+        qstn: 11,
+        tag: "M",
+        ans: 0
+      },
+      {
+        qstn: 12,
+        tag: "M",
+        ans: 1
+      }
+    ]
+  },
+  {
+    test: 2,
+    paper: [
+      {
+        qstn: 1,
+        tag: "P",
+        ans: 0
+      },
+      {
+        qstn: 2,
+        tag: "P",
+        ans: 1
+      },
+      {
+        qstn: 3,
+        tag: "P",
+        ans: 0
+      },
+      {
+        qstn: 4,
+        tag: "P",
+        ans: 1
+      },
+      {
+        qstn: 5,
+        tag: "C",
+        ans: 1
+      },
+      {
+        qstn: 6,
+        tag: "C",
+        ans: 1
+      },
+      {
+        qstn: 7,
+        tag: "C",
+        ans: 1
+      },
+      {
+        qstn: 8,
+        tag: "C",
+        ans: 1
+      },
+      {
+        qstn: 9,
+        tag: "M",
+        ans: 0
+      },
+      {
+        qstn: 10,
+        tag: "M",
+        ans: 0
+      },
+      {
+        qstn: 11,
+        tag: "M",
+        ans: 0
+      },
+      {
+        qstn: 12,
+        tag: "M",
+        ans: 1
+      }
+    ]
+  }
+];
+
+export { profile };
